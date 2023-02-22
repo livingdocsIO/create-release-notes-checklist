bf0e25aedd7b95eab9e3fc6e71d3255dac12771c
Ralph Meier <ralphmeier79@gmail.com>
2023-02-22 09:21:48 +0100

    fix: remove rebase check

--- checklist.js
+++ checklist.js
@@ -39,7 +39,6 @@ const getBody = (releaseName) => {
 
   # NZZ 
   * Check if the downstream integration branch of the **PREVIOUS** release are in a correct state
-    * [ ] Rebase the downstream integration branch (NZZ -> ask Heini)
     * [ ] Check and update the integration notes
   * [ ] [Create downstream integration](https://github.com/upfrontIO/team/blob/master/How-We-Work/Release-Process/Details/Integration-Branches.md) branches of the next release for the NZZ (branch name: upstream-release-MMMM-YY) for the [nzz server](https://github.com/nzzdev/livingdocs-api) and [nzz editor](https://github.com/nzzdev/livingdocs-editor)
   * [ ] Update livingdocs-integration.json of the [server](https://github.com/upfrontIO/livingdocs-server/blob/master/livingdocs-integration.json)/[editor](https://github.com/upfrontIO/livingdocs-editor/blob/master/livingdocs-integration.json)/[framework](https://github.com/upfrontIO/livingdocs-framework/blob/master/livingdocs-integration.json)
