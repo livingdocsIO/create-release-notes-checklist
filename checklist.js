7dca3fb1856c7018f3f53e2e56dcdfd8301b51b4
Ralph Meier <ralphmeier79@gmail.com>
2023-05-02 11:08:08 +0200

    fix: Update checklist.js

--- checklist.js
+++ checklist.js
@@ -37,19 +37,16 @@ const getBody = (releaseName) => {
   * [ ] [Publish](https://github.com/livingdocsIO/livingdocs-framework/blob/master/packages/sdk-prebuild/DEV-README.md) a new \`@livingdocs/sdk-framework-prebuild\` npm version in the framework in \`${releaseName}\` branch and update the [SDK](https://github.com/livingdocsIO/livingdocs-node-sdk) to the generated version in \`${releaseName}\`
   * [ ] Get a list of new Documentation Guides based on the diff between the last 2 releases and give that info to the Marketing team -> \`git diff origin/master@{"06.12.2022"} origin/master -- ./content/guides\`
 
+  # Release Notes
+
+  * [ ] Switch the release config to the next release [see example commit](https://github.com/livingdocsIO/documentation/commit/45d40c711bd75d38edd35cdfec976bce3eb50ddb) but check if the relase notes document already exist!
+
   # NZZ 
   * Check if the downstream integration branch of the **PREVIOUS** release are in a correct state
     * [ ] Check and update the integration notes
   * [ ] [Create downstream integration](https://github.com/upfrontIO/team/blob/master/How-We-Work/Release-Process/Details/Integration-Branches.md) branches of the next release for the NZZ (branch name: upstream-release-MMMM-YY) for the [nzz server](https://github.com/nzzdev/livingdocs-api) and [nzz editor](https://github.com/nzzdev/livingdocs-editor)
   * [ ] Update livingdocs-integration.json of the [server](https://github.com/upfrontIO/livingdocs-server/blob/master/livingdocs-integration.json)/[editor](https://github.com/upfrontIO/livingdocs-editor/blob/master/livingdocs-integration.json)/[framework](https://github.com/upfrontIO/livingdocs-framework/blob/master/livingdocs-integration.json)
     * attention: the \`defaultBranch\` in livingdocs-integration.json of the release branch in the framework must be set to the current release after creating the release branch e.g. [example](https://github.com/livingdocsIO/livingdocs-framework/blob/release-2020-05/livingdocs-integration.json#L4)
-
-  # Preparation (for next cycle)
-
-  * [ ] Create release notes template for next release in the documentation
-    * move master to subfolder \`${releaseName}\`
-    * create new master document (copy from \`${releaseName}\`, because the release-notes-generator does not work properly anymore)
-    * change \`excludeFromSearch\` in upcoming release, as soon as the release is officially out!
   `
 }
 
